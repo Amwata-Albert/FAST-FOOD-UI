105fb9c2d016fa5d12fa447d43029aebe03b7bb5
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-17 14:46:36 +0300

    my commit refine code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; 
+// import { NgProgressModule } from '@ngx-progressbar/core';
+// import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -16,6 +18,8 @@ import { RegisterComponent } from './register/register.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    // NgProgressModule.forRoot(),
+    // NgProgressHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
