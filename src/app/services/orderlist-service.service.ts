e85805a38ddfb165cb1f8f59cb69b69f98336f09
kiira254 <nkamotho57@gmail.com>
2020-11-19 09:06:30 +0300

    add get function

--- src/app/services/orderlist-service.service.ts
+++ src/app/services/orderlist-service.service.ts
@@ -7,6 +7,30 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class OrderlistServiceService {
+  
+  // http options used for making API calls
+  private httpOptions: any;
+  orderlist:any
+  errors:boolean=false
+  
+  constructor(private http:HttpClient,private router:Router) {
+  	this.httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+  }
 
-  constructor() { }
+public getOrder() {
+  this.http.get(`${environment.apiUrl}/api/customer`, this.httpOptions).subscribe(
+    data => {
+      this.orderlist=data
+      console.log(data)       
+    },
+    err => {
+      console.log(err)
+      this.errors=true
+        }
+  );
+}
+
+
 }
