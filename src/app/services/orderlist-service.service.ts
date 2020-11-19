eacd4a2b6bf00353b7268eb2937d14ec86cc6564
kiira254 <nkamotho57@gmail.com>
2020-11-19 08:56:54 +0300

    add imports to thr service

---
+++ src/app/services/orderlist-service.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderlistServiceService {
+
+  constructor() { }
+}
