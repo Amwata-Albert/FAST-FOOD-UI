b8967f61eaf8ed95d77dae70c61953c1fc61bffa
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-24 10:55:25 +0300

    my commit delete items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,51 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component'
-import { NavbarComponent } from './navbar/navbar.component'
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent},
   { path: '', component: LoginComponent},
   { path: 'home', component: NavbarComponent},
+  
 ];
-=======
-import { DefaultComponent } from './layouts/default/default.component';
-import { DashboardComponent } from './module/dashboard/dashboard.component';
-import { CurrentOrdersComponent } from './modules/current-orders/current-orders.component';
-import { CustomersComponent } from './modules/customers/customers.component';
-import { MealsComponent } from './modules/meals/meals.component';
-import { OrderHistoryComponent } from './modules/order-history/order-history.component';
 
-const routes: Routes = [{
-  path: '', 
-  component:  DefaultComponent,
-  children: [
-    {path: '', component: DashboardComponent},
-    {path: 'meals', component: MealsComponent},
-    {path: 'Customers', component: CustomersComponent},
-    {path: 'Current', component: CurrentOrdersComponent},
-    {path: 'order', component: OrderHistoryComponent},
-    
-
-
-    {
-      path: '',
-      redirectTo: 'posts',
-      pathMatch: 'full'
-    }
-  ]
-}];
-
-// const routes: Routes = [
-//   { path: '', component: DefaultComponent,
-//    children:[{
-//     path:'', component: DashboardComponent
-//   }
-// ]},
-// ];
->>>>>>> 165aeae02ab99edec2bcc1eb9315103b6495941b
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-=======
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; 
-
->>>>>>> 165aeae02ab99edec2bcc1eb9315103b6495941b
 import { AppRoutingModule } from './app-routing.module';
-// import { NgProgressModule } from '@ngx-progressbar/core';
-// import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { CustomerComponent } from './customer/customer.component';
-import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
@@ -25,54 +14,15 @@ import { OrdersComponent } from './orders/orders.component';
     RegisterComponent,
     LoginComponent,
     NavbarComponent,
-    CustomerComponent,
-    OrdersComponent,
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DefaultComponent } from './layouts/default/default.component';
-import { DefaultModule } from './layouts/default/default.module';
-// import { MealsComponent } from './modules/meals/meals.component';
-// import { HeaderComponent } from './shared/components/header/header.component';
-// import { FooterComponent } from './shared/components/footer/footer.component';
-// import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-// import { DefaultComponent } from './layouts/default/default.component';
-// import { DashboardComponent } from './module/dashboard/dashboard.component';
-// import { MatButtonModule } from '@angular/material/button';
- 
-@NgModule({
-  declarations: [
-    AppComponent,
     
-    // MealsComponent,
-    // HeaderComponent,
-    // FooterComponent,
-    // SidebarComponent
-    // DefaultComponent,
-    // DashboardComponent
-
->>>>>>> 165aeae02ab99edec2bcc1eb9315103b6495941b
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     HttpClientModule,
-    // NgProgressModule.forRoot(),
-    // NgProgressHttpClientModule
-=======
-    HttpClientModule,
-    FormsModule,
-    NgbModule,
-    BrowserAnimationsModule,
-    DefaultModule
-    // MatButtonModule
->>>>>>> 165aeae02ab99edec2bcc1eb9315103b6495941b
   ],
-  // imports: [
-  //   BrowserModule,
-  //   AppRoutingModule
-  // ],
   providers: [],
   bootstrap: [AppComponent]
 })
